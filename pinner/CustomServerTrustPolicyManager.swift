c44b41846e6a8c25d098e604642830ae70054d3c
Ricardo Maqueda Martinez <rinconestolmo@gmail.com>
2018-10-15 04:07:07 +0100

    Download certificate button

--- pinner/CustomServerTrustPolicyManager.swift
+++ pinner/CustomServerTrustPolicyManager.swift
@@ -14,7 +14,7 @@ class CustomServerTrustPolicyManager: ServerTrustPolicyManager {
     override func serverTrustPolicy(forHost host: String) -> ServerTrustPolicy? {
         // Check if we have a policy already defined, otherwise just kill the connection
         if let policy = super.serverTrustPolicy(forHost: host) {
-            print(policy)
+            //print(policy)
             return policy
         } else {
             return .customEvaluation({ (_, _) -> Bool in
--- pinner/DownloadSessionDelegate.swift
+++ pinner/DownloadSessionDelegate.swift
@@ -20,10 +20,14 @@ class DownloadSessionDelegate: SessionDelegate {
                 return
             }
             
-            if let serverCertificate = SecTrustGetCertificateAtIndex(trust, 0), let serverCertificateKey = self.publicKey(for: serverCertificate) {
+            if let serverCertificate = SecTrustGetCertificateAtIndex(trust, 0),
+                let serverCertificateKey = self.publicKey(for: serverCertificate)
+            {
                 // TODO: Convert and save public key
                 print(serverCertificateKey)
+                print(self.data(publicKey: serverCertificateKey)!)
                 completion(.useCredential, URLCredential(trust: trust))
+                return
             }
             
             completion(.cancelAuthenticationChallenge, nil)
@@ -33,16 +37,28 @@ class DownloadSessionDelegate: SessionDelegate {
     
     private func publicKey(for certificate: SecCertificate) -> SecKey? {
         var publicKey: SecKey?
-        
+
         let policy = SecPolicyCreateBasicX509()
         var trust: SecTrust?
         let trustCreationStatus = SecTrustCreateWithCertificates(certificate, policy, &trust)
-        
+
         if let trust = trust, trustCreationStatus == errSecSuccess {
             publicKey = SecTrustCopyPublicKey(trust)
         }
-        
+
         return publicKey
+        
+//        return SecCertificateCopyPublicKey(certificate)
+    }
+    
+    private func data(publicKey: SecKey) -> String? {
+        var error:Unmanaged<CFError>?
+        if let cfdata = SecKeyCopyExternalRepresentation(publicKey, &error) {
+            let data:Data = cfdata as Data
+            let b64Key = data.base64EncodedString()
+            return b64Key
+        }
+        return nil
     }
     
 }
